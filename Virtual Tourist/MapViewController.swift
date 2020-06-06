7e7438800a38b04416e8e3e35e98a5f19be40c7e
Gerardo Tarazona <getaca@gmail.com>
2020-06-06 10:53:00 -0500

    created animation to transition between pin and photos

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var containerPhotosHeight: NSLayoutConstraint!
+    @IBOutlet weak var containerCollectionView: UIView!
+    lazy var photosViewController: UIViewController = {
+        UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "PhotosViewController")
+    }()
+    
+    var toogled: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func buttonBarPressed() {
+        if toogled {
+            toogled = false
+            UIView.animate(withDuration: 0.5, animations: { [unowned self] in
+                self.containerPhotosHeight.constant = 0
+                self.view.layoutSubviews()
+            }) { [unowned self] (finished) in
+                self.photosViewController.view.removeFromSuperview()
+                self.photosViewController.willMove(toParent: nil)
+                self.photosViewController.removeFromParent()
+            }
+        } else {
+            toogled = true
+            self.addChild(photosViewController)
+            self.containerCollectionView.addSubview(photosViewController.view)
+            self.photosViewController.didMove(toParent: self)
+            
+            UIView.animate(withDuration: 0.5, animations: { [unowned self] in
+                self.containerPhotosHeight.constant = self.view.frame.height/1.4
+                self.view.layoutSubviews()
+            })
+        }
+    }
 }
 
---
+++ Virtual Tourist/PhotosViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PhotosViewController.swift
+//  Virtual Tourist
+//
+//  Created by Gerardo Tarazona Caceres on 6/4/20.
+//  Copyright © 2020 gerardo. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("willappear")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
